f2ef40e2e1b67e8727dec6b69801c62032adabde
Yiyi <yiyiwzhang@gmail.com>
2017-03-08 14:58:15 -0500

    Added nodes to Rail parent.

--- Assets/Scripts/Rail.cs
+++ Assets/Scripts/Rail.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rail : MonoBehaviour {
+	private Vector3[] nodes;
+	private int nodeCount;
 
-	// Use this for initialization
-	void Start () {
-		
+	private void Start(){
+		nodeCount = transform.GetChildCount(); /*counts number of children in transform*/
+		nodes = new Vector3[nodeCount]; /*there will be as many Vector3s as there are children*/
+
+		for(int i = 0; i < nodeCount; i++){
+			nodes[i] = transform.GetChild(i).position; /*get transform for each node iterating from 0 to the number of nodes there are*/
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private void Update(){
+		if (nodeCount > 1) { /*if there's more than one node. if there's only one node, obvs only need to return the position of the one*/
+			for (int i = 0; i < nodeCount - 1; i++) {
+				Debug.DrawLine (nodes [i], nodes [i + 1], Color.green);
+			}
+		}
 	}
+
+	public Vector3 ProjectPositionOnRail(Vector3 pos){ /*compares position of object against relative distance to the distance between nodes*/
+		int closestNodeIndex = 0; /*get closest node*/
+		if (closestNodeIndex == 0){
+			/*Project on first segment*/
+		} else if(closestNodeIndex == nodeCount -1){
+			/*Project on last segment*/
+		} else {
+			/*Project on the 2 connected Segments*/
+			/*Return shortest vector*/
+		}
 }
---
+++ Assets/Scripts/RailMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailMover : MonoBehaviour {
+	public Rail rail;
+	public Transform lookAt; /*tells camera where to be looking*/
+
+	private Transform thisTransform;
+
+	private void Start(){
+		thisTransform = transform;
+	}
+
+}
