f5a7963af7665009a37db21b11b193572b2c6904
Yiyi <yiyiwzhang@gmail.com>
2017-03-14 21:49:58 -0400

    Completed camera follow, having issues with Lerp/camera smoothing (when smoothing is enabled, camera looks at Nodes and not player)

--- Assets/Scripts/Rail.cs
+++ Assets/Scripts/Rail.cs
@@ -24,13 +24,57 @@ public class Rail : MonoBehaviour {
 	}
 
 	public Vector3 ProjectPositionOnRail(Vector3 pos){ /*compares position of object against relative distance to the distance between nodes*/
-		int closestNodeIndex = 0; /*get closest node*/
+		int closestNodeIndex = GetClosestNode(pos); /*get closest node position using function below that spits out the V3 position of the closest node*/
 		if (closestNodeIndex == 0){
-			/*Project on first segment*/
+			return ProjectOnSegment (nodes [0], nodes [1], pos); /*for first segment*/
 		} else if(closestNodeIndex == nodeCount -1){
-			/*Project on last segment*/
+			return ProjectOnSegment (nodes [nodeCount - 1], nodes [nodeCount - 2], pos); /*for first segment*/
 		} else {
+			Vector3 leftSeg = ProjectOnSegment (nodes [closestNodeIndex - 1], nodes [closestNodeIndex], pos);
+			Vector3 rightSeg = ProjectOnSegment (nodes [closestNodeIndex + 1], nodes [closestNodeIndex], pos);
+
+			Debug.DrawLine (pos, leftSeg, Color.red);
+			Debug.DrawLine (pos, rightSeg, Color.blue);
+
+			if ((pos - leftSeg).sqrMagnitude <= (pos - rightSeg).sqrMagnitude) {
+				return leftSeg;
+
+			} else {
+				return rightSeg;
+			}
 			/*Project on the 2 connected Segments*/
 			/*Return shortest vector*/
 		}
+	}
+
+	private int GetClosestNode(Vector3 pos){ /*getting position of the closest node to the object*/
+		int closestNodeIndex = -1;
+		float shortestDistance = 0.0f;
+
+		for(int i = 0; i < nodeCount; i++){
+			float sqrDistance = (nodes[i] - pos).sqrMagnitude; /*calculating distance of nodes in square to save memory*/
+			if(shortestDistance == 0.0f || sqrDistance < shortestDistance){
+				shortestDistance = sqrDistance;
+				closestNodeIndex = i;
+			}
+		}
+		return closestNodeIndex;
+	}
+
+	private Vector3 ProjectOnSegment(Vector3 v1, Vector3 v2, Vector3 pos){
+		Vector3 v1ToPos = pos - v1;
+		Vector3 segDirection = (v2 - v1).normalized; /*to get the intersection point between the two vectors*/
+
+		float distanceFromV1 = Vector3.Dot(segDirection,v1ToPos); /*distance from the 1st point to where we should be on the line*/
+		if(distanceFromV1 < 0.0f){
+			return v1;
+		} else if (distanceFromV1*distanceFromV1 > (v2-v1).sqrMagnitude){
+			return v2;
+		} else {
+			Vector3 fromV1 = segDirection * distanceFromV1;
+			return v1 + fromV1;
+		}
+	}
 }
+
+//14:32 https://www.youtube.com/watch?v=B3LYJINnYtE&feature=youtu.be
--- Assets/Scripts/RailMover.cs
+++ Assets/Scripts/RailMover.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class RailMover : MonoBehaviour {
 	public Rail rail;
 	public Transform lookAt; /*tells camera where to be looking*/
+	public bool smoothMove = true; /*for smoothing camera transition between nodes*/
+	public float moveSpeed = 5.0f; /*camera transition speed*/
 
 	private Transform thisTransform;
+	private Vector3 lastPosition; /*the position of the camera just before jumping to the next node*/
 
 	private void Start(){
 		thisTransform = transform;
+		lastPosition = thisTransform.position;
 	}
 
+	private void Update(){
+		if (smoothMove) {
+			lastPosition = Vector3.Lerp(lastPosition, rail.ProjectPositionOnRail (lookAt.position), Time.deltaTime * moveSpeed);
+			thisTransform.position = lastPosition;
+		} else {
+			thisTransform.position = rail.ProjectPositionOnRail (lookAt.position); /*look at where the thing is on the rail*/
+			thisTransform.LookAt (lookAt.position); /*look at the player*/
+		}
+	}
 }
